fd3cba23c50a05516542ac5360643c633376dcf4
Viktor Ruskai <viktor@ruskai.com>
2022-03-14 15:09:42 +0100

    fix(tests): fix environment for integration tests

--- tests/create-environment-for-testing.sh
+++ tests/create-environment-for-testing.sh
@@ -9,22 +9,22 @@ if [[ "$LARAVEL_VERSION" = "" ]]; then
 fi
 
 echo ">> Install Laravel"
-composer create-project --prefer-dist laravel/laravel:$LARAVEL_VERSION ./laravel || exit 1
-cd ./laravel
+composer create-project --prefer-dist laravel/laravel:$LARAVEL_VERSION ../laravel || exit 1
+cd ../laravel
 
 echo ">> Install Faker"
 composer require fakerphp/faker
 
 echo "Add package from source"
 sed -e 's|"type": "project",|&\n"repositories": [ { "type": "path", "url": "../advanced-upsert-for-laravel" } ],|' -i composer.json || exit 1
-composer require "viktorruskai/advanced-upsert-for-laravel:*" || exit 1
+composer require --dev "viktorruskai/advanced-upsert-for-laravel:*" || exit 1
 
 echo ">> Copy all required files"
-cp ../tests/Support/Models/* ./app/Models && echo "\xE2\x9C\x94 Models" || exit 1
-cp ../tests/Support/Migrations/* ./database/migrations && echo "\xE2\x9C\x94 Migrations" || exit 1
-cp ../tests/Support/Factories/* ./database/factories && echo "\xE2\x9C\x94 Factories" || exit 1
+cp ../advanced-upsert-for-laravel/tests/Support/Models/* ./app/Models && echo "\xE2\x9C\x94 Models" || exit 1
+cp ../advanced-upsert-for-laravel/tests/Support/Migrations/* ./database/migrations && echo "\xE2\x9C\x94 Migrations" || exit 1
+cp ../advanced-upsert-for-laravel/tests/Support/Factories/* ./database/factories && echo "\xE2\x9C\x94 Factories" || exit 1
 mkdir -m755 ./app/Console/Commands
-cp ../tests/Support/Commands/* ./app/Console/Commands && echo "\xE2\x9C\x94 Commands" || exit 1
+cp ../advanced-upsert-for-laravel/tests/Support/Commands/* ./app/Console/Commands && echo "\xE2\x9C\x94 Commands" || exit 1
 
 
 # todo: mozno vyskaut Commands/* ???? ked to prekopiruje tak sa spravi z toho subor a nie directory
