e78d26354d4b5cd05175459302e3fd668c137bd2
Viktor Ruskai <viktor@ruskai.com>
2022-03-14 19:57:05 +0100

    fix(tests): fix environment for integration tests

--- tests/create-environment-for-testing.sh
+++ tests/create-environment-for-testing.sh
@@ -25,7 +25,7 @@ cp ../advanced-upsert-for-laravel/tests/Support/Migrations/* ./database/migratio
 cp ../advanced-upsert-for-laravel/tests/Support/Factories/* ./database/factories && echo "\xE2\x9C\x94 Factories" || exit 1
 mkdir -m755 ./app/Console/Commands
 cp ../advanced-upsert-for-laravel/tests/Support/Commands/* ./app/Console/Commands && echo "\xE2\x9C\x94 Commands" || exit 1
-cp ../advanced-upsert-for-laravel/tests/Support/Tests/* ./tests/ && echo "\xE2\x9C\x94 Commands" || exit 1
+cp ../advanced-upsert-for-laravel/tests/Support/Tests/* ./tests/Unit && echo "\xE2\x9C\x94 Tests" || exit 1
 
 echo ">> Migrate"
 php artisan migrate
