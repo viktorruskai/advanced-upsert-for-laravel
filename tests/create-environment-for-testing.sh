f403cac65e94ec4d97a6dc2bfc920c287c5be508
Viktor Ruskai <viktor@ruskai.com>
2022-03-14 12:30:27 +0100

    fix(tests): fix environment for integration tests

--- tests/create-environment-for-testing.sh
+++ tests/create-environment-for-testing.sh
@@ -12,15 +12,10 @@ echo ">> Install Laravel"
 composer create-project --prefer-dist laravel/laravel:$LARAVEL_VERSION ./laravel || exit 1
 cd ./laravel
 
-cd ./app/Console
-ls -la
-cd ./Commands
-ls -la
-cd ../../../
-
 echo ">> Copy all required files"
 cp ../tests/Support/Models/* ./app/Models && echo "\xE2\x9C\x94 Models" || exit 1
 cp ../tests/Support/Migrations/* ./database/migrations && echo "\xE2\x9C\x94 Migrations" || exit 1
+mkdir -m755 ./app/Console/Commands
 cp ../tests/Support/Commands/* ./app/Console/Commands && echo "\xE2\x9C\x94 Commands" || exit 1
 
 
