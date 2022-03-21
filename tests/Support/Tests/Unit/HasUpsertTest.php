0407e7974bd581226fb35f7e18f1d1aa4cd21e54
Viktor Ruskai <viktor@ruskai.com>
2022-03-21 20:04:11 +0100

    fix(HasUpsertTest): fix `upsertFunction` function

--- tests/Support/Tests/Unit/HasUpsertTest.php
+++ tests/Support/Tests/Unit/HasUpsertTest.php
@@ -9,7 +9,6 @@ namespace Tests\Unit;
 
 use App\Models\Item;
 use App\Models\ItemAction;
-use App\Models\ItemActionAdditional;
 use Illuminate\Database\Query\Expression;
 use Illuminate\Database\QueryException;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
@@ -24,7 +23,7 @@ use Tests\TestCase;
  */
 class HasUpsertTest extends TestCase
 {
-    use DatabaseMigrations;
+    use DatabaseMigrations, DataProviders;
 
     /**
      * Must be tested by inserting row into the DB.
@@ -55,67 +54,6 @@ class HasUpsertTest extends TestCase
         );
 
         $this->assertSame(DB::getQueryLog()[0]['query'] ?? [], $expected);
-    }
-
-    /**
-     * @noinspection SqlDialectInspection
-     * @noinspection SqlNoDataSourceInspection
-     */
-    public function upsertDataProvider(): array
-    {
-        return [
-            [
-                ItemAction::class,
-                [
-                    [
-                        'itemId' => 1,
-                        'actionName' => 'Test',
-                        'actionDescription' => 'Test description',
-                    ],
-                ],
-                ['itemId', 'actionName'], // Conflict
-                ['actionDescription'], // Update
-                null, // Selected model,
-                [],
-                'INSERT INTO "itemActions" ("itemId", "actionName", "actionDescription", "updatedAt", "createdAt") VALUES (1,\'Test\',\'Test description\',NOW(),NOW()) ON CONFLICT ("itemId", "actionName") DO UPDATE SET "actionDescription" = "excluded"."actionDescription"',
-            ],
-            [
-                ItemAction::class,
-                [
-                    [
-                        'itemId' => 1,
-                        'actionName' => 'Test',
-                        'actionDescription' => 'Test description',
-                    ],
-                ],
-                'custom_unique_key_for_item_actions', // Conflict
-                ['actionDescription'], // Update
-                null, // Selected model,
-                [],
-                'INSERT INTO "itemActions" ("itemId", "actionName", "actionDescription", "updatedAt", "createdAt") VALUES (1,\'Test\',\'Test description\',NOW(),NOW()) ON CONFLICT ON CONSTRAINT custom_unique_key_for_item_actions DO UPDATE SET "actionDescription" = "excluded"."actionDescription"',
-            ],
-            [
-                ItemActionAdditional::class,
-                [
-                    [
-                        'where' => [
-                            'itemId' => 1,
-                            'actionName' => 'Test',
-                        ],
-                        'upsert' => [
-                            'itemActionId' => '*',
-                            'specialData' => '123456',
-                            'description' => 'Hello',
-                        ],
-                    ],
-                ],
-                ['itemActionId', 'specialData'], // Conflict
-                ['description'], // Update
-                ItemAction::class, // Selected model
-                [],
-                'INSERT INTO "itemActionAdditional" ("itemActionId", "specialData", "description", "updatedAt", "createdAt") (SELECT id,\'123456\',\'Hello\',NOW(),NOW() FROM "itemActions" WHERE "itemId" = 1 AND "actionName" = \'Test\') ON CONFLICT ("itemActionId", "specialData") DO UPDATE SET "description" = "excluded"."description"',
-            ],
-        ];
     }
 
     /**
@@ -150,6 +88,37 @@ class HasUpsertTest extends TestCase
     }
 
     /**
+     * @throws ReflectionException
+     */
+    public function testInvalidConflictConstraintInUpsertFunction(): void
+    {
+        $this->expectException(QueryException::class);
+
+        Item::create([
+            'name' => 'Test',
+            'description' => 'Test Description',
+        ]);
+
+        DB::enableQueryLog();
+
+        $itemActionMock = $this->partialMock(ItemAction::class);
+
+        $upsertFunction = new ReflectionMethod(ItemAction::class, 'upsert');
+        $upsertFunction->invoke(
+            $itemActionMock,
+            [
+                [
+                    'itemId' => 1,
+                    'actionName' => 'Test',
+                    'actionDescription' => 'Test description',
+                ],
+            ],
+            'random_unique_key_name',
+            ['description']
+        );
+    }
+
+    /**
      * @dataProvider compileInsertDataProvider
      * @throws ReflectionException
      */
@@ -172,38 +141,6 @@ class HasUpsertTest extends TestCase
     }
 
     /**
-     * @noinspection SqlDialectInspection
-     * @noinspection SqlNoDataSourceInspection
-     */
-    public function compileInsertDataProvider(): array
-    {
-        return [
-            [
-                [
-                    [
-                        'actionName' => 'Test',
-                        'actionDescription' => 'Test description',
-                    ],
-                ], null, 'INSERT INTO "itemActions" ("actionName", "actionDescription", "updatedAt", "createdAt") VALUES (\'Test\',\'Test description\',NOW(),NOW())'
-            ],
-            [
-                [
-                    [
-                        'where' => [
-                            'actionId' => 1,
-                            'actionName' => 'Test',
-                        ],
-                        'upsert' => [
-                            'specialData' => '123456',
-                            'description' => 'Hello',
-                        ],
-                    ],
-                ], ItemActionAdditional::class, 'INSERT INTO "itemActions" ("specialData", "description", "updatedAt", "createdAt") (SELECT \'123456\',\'Hello\',NOW(),NOW() FROM "itemActionAdditional" WHERE "actionId" = 1 AND "actionName" = \'Test\')',
-            ],
-        ];
-    }
-
-    /**
      * @dataProvider compileUpdateDataProvider
      * @throws ReflectionException
      */
@@ -221,20 +158,6 @@ class HasUpsertTest extends TestCase
         );
 
         $this->assertSame($returnedUpdatedString, $expected);
-    }
-
-    public function compileUpdateDataProvider(): array
-    {
-        return [
-            [[
-                'actionDescription',
-                'testField' => 1,
-            ], ' DO UPDATE SET "actionDescription" = "excluded"."actionDescription", "testField" = ?'],
-            [[
-                'actionDescription' => 'Test 123',
-                'testField',
-            ], ' DO UPDATE SET "actionDescription" = ?, "testField" = "excluded"."testField"'],
-        ];
     }
 
     /**
@@ -328,16 +251,6 @@ class HasUpsertTest extends TestCase
         );
 
         $this->assertSame($returnedParsedValue, $expected);
-    }
-
-    public function valueDataProvider(): array
-    {
-        return [
-            [0.5, '0.5'],
-            [null, ''],
-            ['test', '\'test\''],
-            [new Expression('NOW()'), '\'NOW()\''],
-        ];
     }
 
     /**
